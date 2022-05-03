e18b6c12aa2e0a41ee1840b3d558689fc75acb39
jwmatson <jwmatson@ualberta.ca>
2022-05-02 18:13:09 -0600

    inheritance, dynamic urls

--- djangoPractice/meetups/urls.py
+++ djangoPractice/meetups/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [ #has to be written exactly like this
-    path('meetups/', views.index ) #domain.com/meetups
+    path('meetups/', views.index, name='all-meetups' ), #domain.com/meetups
+    path('meetups/<slug:meetup_slug>', views.meetup_details, name='meetup-detail') #dynamic url     #domain.com/meetups/<dynamic-meetup-title> etc.
+    
 ] 
--- djangoPractice/meetups/views.py
+++ djangoPractice/meetups/views.py
@@ -1,3 +1,4 @@
+from turtle import title
 from django.shortcuts import render
 
 
@@ -5,10 +6,22 @@ from django.shortcuts import render
 
 def index(request):
     meetups= [
-        {'title': 'A First Meetup'},
-        {'title': 'A Second Meetup'},
+        {'title': 'A First Meetup', 
+        'location': 'New York', 
+        'slug': 'a-first-meetup'},
+
+        {'title': 'A Second Meetup', 
+        'location': 'Paris',
+        'slug': 'a-second-meetup'},
     ]
-    return render(request, 'meetups/index.html', {
-      'show_meetups': False,
-      'meetups':meetups  
+    return render(request, 'meetups/index.html', { #return rendered template
+      'show_meetups': True,
+      'meetups':meetups  #this is what is being passed into the html to be manipualted with {}
     })
+
+def meetup_details(request, meetup_slug): #meetup_slug needs to match the identifier in urls
+  selected_meetup = {'title': 'A first Meetup', 
+                    'description':'This is the first meetup!'}
+  return render(request, 'meetups/meetup-details.html', {
+    'meetup_details':selected_meetup
+  })
